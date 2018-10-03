d6ee588685c0b367224283a6b4270dc0bdff565f
David Haworth <dh@thelancashireman.org>
2018-10-03 17:47:15 +0200

    Split the "logic analyser" part out to a separate header file.

---
+++ PinCapture.h
@@ -0,0 +1,104 @@
+/* PinCapture - the logic analyser bits of qdscope.
+ *
+ * (c) 2018 David Haworth
+ *
+ * QdScope is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QdScope is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QdScope.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * qdscope is an Arduino sketch, written for an Arduino Nano
+*/
+
+/* Capturing inputs.
+ *
+ * ToDo: make the number and selection of pins configurable.
+*/
+#define NCAP 50
+#define inputPin0	8		// PB0
+#define inputPin1	9		// PB1
+#define inputPin2	10		// PB2
+#define inputPin3	11		// PB3
+
+/* These are the data types used in this module.
+*/
+typedef unsigned char capture_t;			/* Allows up to 8 pins */
+
+typedef struct pincapture_s pincapture_t;	/* Structure hold time and (changed) state)\ */
+struct pincapture_s
+{
+	unsigned long capTime;
+	capture_t capValue;
+};
+
+/* Static/global variables
+*/
+capture_t oldPins;							/* Previous state of the pins. We only store changes. */
+pincapture_t pinCapture[NCAP];				/* Array of captures */
+int capIndex;								/* Position in the array */
+
+static inline capture_t PinCapture_ReadInputs(void)
+{
+	capture_t v = 0;
+    if ( digitalRead(inputPin3) == HIGH )	v |= 0x08;
+    if ( digitalRead(inputPin2) == HIGH )	v |= 0x04;
+    if ( digitalRead(inputPin1) == HIGH )	v |= 0x02;
+    if ( digitalRead(inputPin0) == HIGH )	v |= 0x01;
+	return v;
+}
+
+static inline void PinCapture_Dump(void)
+{
+	int i;
+	capture_t v;
+
+	for ( i = 0; i < capIndex; i++ )
+	{
+		Serial.print(pinCapture[i].capTime);
+		Serial.print(' ');
+		v = pinCapture[i].capValue;
+		Serial.print((v&0x08)?" 1":" 0");
+		Serial.print((v&0x04)?" 1":" 0");
+		Serial.print((v&0x02)?" 1":" 0");
+		Serial.print((v&0x01)?" 1":" 0");
+		Serial.println("");
+	}
+
+	capIndex = 0;
+}
+
+void PinCapture_Loop(void)
+{
+	capture_t newPins = PinCapture_ReadInputs();
+
+	if ( oldPins != newPins )
+	{
+		pinCapture[capIndex].capTime = micros();
+		pinCapture[capIndex].capValue = newPins;
+		oldPins = newPins;
+
+		capIndex++;
+		if ( capIndex >= NCAP )
+		{
+			PinCapture_Dump();
+		}
+	}
+}
+
+static inline void PinCapture_Setup(void)
+{
+	pinMode(inputPin0, INPUT_PULLUP); 			//	Set up the input pins
+	pinMode(inputPin1, INPUT_PULLUP); 
+	pinMode(inputPin2, INPUT_PULLUP); 
+	pinMode(inputPin3, INPUT_PULLUP); 
+	oldPins = PinCapture_ReadInputs();
+	capIndex = 0;
+}
--- qdscope.ino
+++ qdscope.ino
@@ -2,23 +2,23 @@
  *
  * (c) 2016 David Haworth
  *
- * lcdTerminal is free software: you can redistribute it and/or modify
+ * QdScope is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * lcdTerminal is distributed in the hope that it will be useful,
+ * QdScope is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with DhG.  If not, see <http://www.gnu.org/licenses/>.
- *
- * $Id$
+ * along with QdScope.  If not, see <http://www.gnu.org/licenses/>.
  *
  * qdscope is an Arduino sketch, written for an Arduino Nano
 */
+
+#include "PinCapture.h" 
 
 /* Blinkenlight
 */
@@ -29,35 +29,10 @@ char ledState;
 */
 unsigned long then;
 
-/* Capturing inputs
-*/
-#define NCAP 50
-#define inputPin0	8		// PB0
-#define inputPin1	9		// PB1
-#define inputPin2	10		// PB2
-#define inputPin3	11		// PB3
-
-typedef unsigned char capture_t;
-capture_t oldPin;
-
-typedef struct pincapture_s pincapture_t;
-struct pincapture_s
-{
-	unsigned long capTime;
-	capture_t capValue;
-};
-pincapture_t pinCapture[NCAP];
-int capIndex;
-
-capture_t ReadInputs(void);
-void DumpCapture(void);
-
 /* setup() - standard Arduino "Init Task"
 */
 void setup(void)
 {
-	int i;
-
 	pinMode(led1, OUTPUT);				// Sets the digital pin as output
 	digitalWrite(led1, LOW);			// Drive the pin low (LED off)
 	ledState = 0;
@@ -67,12 +42,7 @@ void setup(void)
 
 	then = millis();					// Initialise the time reference.
 
-	pinMode(inputPin0, INPUT_PULLUP); 			//	Set up the input pins
-	pinMode(inputPin1, INPUT_PULLUP); 
-	pinMode(inputPin2, INPUT_PULLUP); 
-	pinMode(inputPin3, INPUT_PULLUP); 
-	oldPin = ReadInputs();
-	capIndex = 0;
+	PinCapture_Setup();
 }
 
 /* loop() - standard Arduino "Background Task"
@@ -81,7 +51,6 @@ void loop(void)
 {
 	unsigned long now = millis();
 	unsigned long elapsed = now - then;
-	capture_t newPin;
 
 	if ( ledState )
 	{
@@ -100,55 +69,15 @@ void loop(void)
 		*/
 		if ( elapsed >= 1980 )
 		{
+			/* This branch gets executed every 2 seconds.
+			*/
 			then += 1980;
 			ledState = 1;
 			digitalWrite(led1, HIGH);
-			DumpCapture();
+			PinCapture_Dump();
+			Serial.println("woo");
 		}
 	}
 
-	newPin = ReadInputs();
-	if ( oldPin != newPin )
-	{
-		pinCapture[capIndex].capTime = micros();
-		pinCapture[capIndex].capValue = newPin;
-		oldPin = newPin;
-
-		capIndex++;
-		if ( capIndex >= NCAP )
-		{
-			DumpCapture();
-		}
-	}
-}
-
-capture_t ReadInputs(void)
-{
-	capture_t v = 0;
-    if ( digitalRead(inputPin3) == HIGH )	v |= 0x08;
-    if ( digitalRead(inputPin2) == HIGH )	v |= 0x04;
-    if ( digitalRead(inputPin1) == HIGH )	v |= 0x02;
-    if ( digitalRead(inputPin0) == HIGH )	v |= 0x01;
-
-	return v;
-}
-
-void DumpCapture(void)
-{
-	int i;
-	capture_t v;
-
-	for ( i = 0; i < capIndex; i++ )
-	{
-		Serial.print(pinCapture[i].capTime);
-		Serial.print(' ');
-		v = pinCapture[i].capValue;
-		Serial.print((v&0x08)?" 1":" 0");
-		Serial.print((v&0x04)?" 1":" 0");
-		Serial.print((v&0x02)?" 1":" 0");
-		Serial.print((v&0x01)?" 1":" 0");
-		Serial.println("");
-	}
-
-	capIndex = 0;
+	PinCapture_Loop();
 }
